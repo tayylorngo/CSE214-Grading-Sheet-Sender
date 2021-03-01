b3b5026d7b494cb331802f03be7a80c358a5b189
Taylor Ngo <47530918+tayylorngo@users.noreply.github.com>
2021-03-01 16:35:48 -0500

    can send xlsx files

--- sendgrades.py
+++ sendgrades.py
@@ -1,17 +1,31 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 import smtplib
-from email.message import EmailMessage
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 
 load_dotenv(find_dotenv())
 EMAIL_ADDRESS = os.getenv('EMAIL_ADDRESS')
 PASSWORD = os.getenv('PASSWORD')
 
-msg = EmailMessage()
-msg['Subject'] = "CSE 214 Homework 2 Grading Sheet"
+body = 'SOMETHING 2'
+
+msg = MIMEMultipart()
 msg['From'] = EMAIL_ADDRESS
 msg['To'] = 'thecse214ta@gmail.com'
-msg.set_content('TESTING1234')
+msg['Subject'] = "CSE 214 Homework 2 Grading Sheet"
+
+msg.attach(MIMEText(body, 'plain'))
+
+filename = 'gradingsheets/CSE214RecitationParticipation.xlsx'
+attachment = open(filename, 'rb')
+part = MIMEBase('application', 'octet-stream')
+part.set_payload(attachment.read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename= "+filename)
+msg.attach(part)
 
 with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
     smtp.ehlo()
@@ -21,3 +35,4 @@ with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
     smtp.login(EMAIL_ADDRESS, PASSWORD)
 
     smtp.send_message(msg)
+    smtp.close()
