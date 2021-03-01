70b26ba1f7cb65eec660f6e846a048b225a3aec1
Taylor Ngo <47530918+tayylorngo@users.noreply.github.com>
2021-03-01 15:48:10 -0500

    setup email connection

--- sendgrades.py
+++ sendgrades.py
@@ -1,9 +1,23 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 import smtplib
+from email.message import EmailMessage
 
 load_dotenv(find_dotenv())
 EMAIL_ADDRESS = os.getenv('EMAIL_ADDRESS')
 PASSWORD = os.getenv('PASSWORD')
 
+msg = EmailMessage()
+msg['Subject'] = "CSE 214 Homework 2 Grading Sheet"
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = 'thecse214ta@gmail.com'
+msg.set_content('TESTING1234')
 
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+
+    smtp.login(EMAIL_ADDRESS, PASSWORD)
+
+    smtp.send_message(msg)
